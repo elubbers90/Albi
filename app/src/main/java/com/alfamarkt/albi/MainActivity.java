328a4bb342d2d2632cc1921c7e979bb970753187
elubbers90 <e.lubbers90@gmail.com>
2015-05-07 00:28:50 +0200

    Reading excel

--- app/src/main/java/com/alfamarkt/albi/MainActivity.java
+++ app/src/main/java/com/alfamarkt/albi/MainActivity.java
@@ -1,17 +1,69 @@
 package com.alfamarkt.albi;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import jxl.*;
+import jxl.read.biff.BiffException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            List<String> result = loadXML("planogram25032015.xls");
+            if(!result.isEmpty()) {
+                Button startButton = (Button) findViewById(R.id.btnStartGame);
+                startButton.setVisibility(View.VISIBLE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> loadXML(String inputFile) throws IOException {
+        List<String> resultSet = new ArrayList<>();
+
+        InputStream in = getAssets().open(inputFile);
+
+        if (in!=null){
+            Workbook w;
+            try {
+                w = Workbook.getWorkbook(in);
+                // Get the first sheet
+                Sheet sheet = w.getSheet(0);
+                // Loop over column and lines
+                for (int j = 0; j < sheet.getRows(); j++) {
+                    for (int i = 0; i < sheet.getColumns(); i++) {
+                        Cell cel = sheet.getCell(i, j);
+                        resultSet.add(cel.getContents());
+                    }
+                }
+            } catch (BiffException e) {
+                e.printStackTrace();
+            }
+        } else {
+            resultSet.add("File not found..!");
+        }
+        if (resultSet.size() == 0) {
+            resultSet.add("Data not found..!");
+        }
+        return resultSet;
     }
 
     @Override
